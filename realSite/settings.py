af875f2ef6844228c6ac72c5123eb4ba2103bafb
Saurabh <saurabh.anil12@gmail.com>
2019-04-19 20:54:04 +0530

    "Fourth commit"

--- realSite/settings.py
+++ realSite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'realtors.apps.RealtorsConfig',
     'django.contrib.humanize',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+#Email config
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER='picameratest123@gmail.com'
+EMAIL_HOST_PASSWORD='picameratest321'
+EMAIL_USE_TLS=True
